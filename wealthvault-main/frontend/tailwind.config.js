b039a1736bbe8aa6c197c1d1747056f12e9724bf
giridharan2004 <160169444+giridharan2004@users.noreply.github.com>
2026-06-17 06:20:18 +0000

    17168318: AM02611:50 AM168 18AMve {{17AM11:50 AMe}}

---
+++ wealthvault-main/frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+﻿/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#00C89B',
+        'primary-light': '#E8F8F5',
+        'bg-light': '#F9FDFC',
+      },
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
